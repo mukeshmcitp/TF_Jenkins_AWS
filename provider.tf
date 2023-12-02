9ef7fbfbd3c9f8961ef0c0b53024d7215f4b21f3
mukeshmcitp <31208972+mukeshmcitp@users.noreply.github.com>
2023-12-02 11:25:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA2HB56W5COWRKGWNF"
-  secret_key = "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"
+  access_key = "AKIA2W5COWRKGWNF"
+  secret_key = "jy3vFL+C9hlanbeWNy32I03JTzVCP"
 }
